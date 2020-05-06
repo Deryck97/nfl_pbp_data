e1f36841de0e8767ff633b1c337f5e2485c753b9
Deryck97 <38873110+Deryck97@users.noreply.github.com>
2020-05-06 12:38:39 -0700

    Create read_nflfastR.py

---
+++ read_nflfastR.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+#Enter desired years of data
+YEARS = [2019,2018,2017]
+
+data = pd.DataFrame()
+
+for i in YEARS:  
+    #low_memory = False eliminates a warning
+    i_data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/compressed_csvs/' \
+                         'data/play_by_play_' + str(i) + '.csv.gz?raw=true',
+                         compression='gzip', low_memory=False)
+
+    #sort=True eliminates a warning and alphabetically sorts columns
+    data = data.append(i_data, sort=True)
