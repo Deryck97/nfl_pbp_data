04ffc6970aedaa3352e577548f76d6fcae158255
Deryck97 <38873110+Deryck97@users.noreply.github.com>
2020-05-06 12:40:03 -0700

    Update read_nflfastR.py

--- read_nflfastR.py
+++ read_nflfastR.py
@@ -6,7 +6,7 @@ YEARS = [2019,2018,2017]
 data = pd.DataFrame()
 
 for i in YEARS:  
-    #low_memory = False eliminates a warning
+    #low_memory=False eliminates a warning
     i_data = pd.read_csv('https://github.com/guga31bb/nflfastR-data/blob/compressed_csvs/' \
                          'data/play_by_play_' + str(i) + '.csv.gz?raw=true',
                          compression='gzip', low_memory=False)
