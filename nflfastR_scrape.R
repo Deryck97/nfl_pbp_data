c816d2f93e9ba3761d8cbb9646ea82896a56abc0
Deryck97 <38873110+Deryck97@users.noreply.github.com>
2020-08-31 17:46:32 -0700

    Create nflfastR_scrape.R

---
+++ nflfastR_scrape.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(nflfastR)
+
+#Specify season
+season = 2020
+
+#Get game ids
+game_ids <- fast_scraper_schedules(season) %>% 
+  filter(game_type=='REG') %>% 
+  pull(game_id)
+
+#Scrape Data
+pbp <- fast_scraper(game_ids, pp=TRUE)
+
+#Apply cleaning function
+cleaned_pbp <- clean_pbp(pbp)
+
+#Optional - Change play types to match if pass or rush == 1
+cleaned_pbp$play_type[cleaned_pbp$pass==1] <- "pass"
+cleaned_pbp$play_type[cleaned_pbp$rush==1] <- "run"
+
+#Put file path in quotes (C:/Users/Name/Documents/pbp_2020.csv.gz)
+#Use file extension .csv.gz for a compressed CSV
+my_path = paste0('FILE PATH')
+
+write.csv(cleaned_pbp, file=gzfile(my_path), row.names=FALSE)
